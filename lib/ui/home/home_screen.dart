e14ae9777fea0914af488dd3c2425ef0c8c2376f
Marcos Roberto Albrecht <marquinhos.beto@hotmail.com>
2025-08-19 15:00:08 -0300

    feat: create bottom navbar, movies and home screen

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:cinebox/ui/home/widgets/home_bottom_nav_bar.dart';
+import 'package:cinebox/ui/movies/movies_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -12,9 +14,13 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home page'),
+      extendBody: true,
+      body: AnimatedSwitcher(
+        duration: Duration(milliseconds: 400),
+        transitionBuilder: (child, animation) => FadeTransition(opacity: animation, child: child),
+        child: MoviesScreen(),
       ),
+      bottomNavigationBar: HomeBottomNavBar(),
     );
   }
 }
