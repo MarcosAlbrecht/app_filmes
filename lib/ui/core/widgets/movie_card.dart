c084fbb9c543193479e0062235d465f8c5d07266
Marcos Roberto Albrecht <marquinhos.beto@hotmail.com>
2025-08-21 11:51:45 -0300

    feat: create movie box

---
+++ lib/ui/core/widgets/movie_card.dart
@@ -0,0 +1,95 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cinebox/ui/core/themes/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class MovieCard extends ConsumerStatefulWidget {
+  const MovieCard({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _MovieCardState();
+}
+
+class _MovieCardState extends ConsumerState<MovieCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        SizedBox(
+          width: 148,
+          height: 250,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CachedNetworkImage(
+                imageUrl: 'https://upload.wikimedia.org/wikipedia/pt/6/63/Joker_%282019%29.jpg',
+                imageBuilder: (context, imageProvider) {
+                  return Container(
+                    width: 148,
+                    height: 184,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
+                    ),
+                  );
+                },
+                placeholder: (context, url) => Center(
+                  child: CircularProgressIndicator(),
+                ),
+                errorWidget: (context, url, error) {
+                  return Container(
+                    width: 148,
+                    height: 184,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.grey,
+                    ),
+                    child: Icon(
+                      Icons.error,
+                      color: Colors.red,
+                    ),
+                  );
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                'You are Conringa?',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w600,
+                ),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+              Text(
+                '2019',
+                style: TextStyle(fontSize: 11, fontWeight: FontWeight.w400, color: AppColors.lightGrey),
+              ),
+            ],
+          ),
+        ),
+        Positioned(
+          right: 0,
+          bottom: 50,
+          child: Material(
+            elevation: 4,
+            borderRadius: BorderRadius.circular(30),
+            child: CircleAvatar(
+              radius: 20,
+              backgroundColor: Colors.white,
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.favorite_border,
+                  size: 16,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
